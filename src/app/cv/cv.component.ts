797fc77d690636fb05f12df8acdd31fa3feee498
Elstryker <rubencorreiacerqueira@gmail.com>
2022-11-08 23:30:33 +0000

    Headline and contacts section on CV

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cv',
   templateUrl: './cv.component.html',
-  styleUrls: ['./cv.component.css']
+  styleUrls: ['./cv.component.css'],
 })
 export class CvComponent implements OnInit {
 
-  constructor() { }
+  public imgPath = './../assets/photo.jpeg'
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
