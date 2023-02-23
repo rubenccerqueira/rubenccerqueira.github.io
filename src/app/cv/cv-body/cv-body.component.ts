5a4ee377210ec528a9d6a8fa27dcf3eaea37db58
Elstryker <rubencorreiacerqueira@gmail.com>
2023-02-23 23:51:16 +0000

    minor fixes

--- src/app/cv/cv-body/cv-body.component.ts
+++ src/app/cv/cv-body/cv-body.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import * as bodyData from './../../data/body.json';
+import * as bodyData from './../../data/cvBody.json';
 
 @Component({
   selector: 'app-cv-body',
   templateUrl: './cv-body.component.html',
-  styleUrls: ['./cv-body.component.scss'],
+  styleUrls: ['./cv-body.component.scss']
 })
 export class CvBodyComponent implements OnInit {
   public bodyData = bodyData;
